bd351d0a8e554d89fec1c36f3042b77868e3ec9f
Nitin Kumar <112533120+nitinkumar9877@users.noreply.github.com>
2025-09-23 09:40:14 +0530

    day09

--- day09/index.js
+++ day09/index.js
@@ -1,95 +1,83 @@
 const express = require("express");
 const app = express();
 
-const bookStore = [
-    { id: 1, name: "Harry Poter", author: "DevFlex" },
-    { id: 2, name: "Spider man", author: "Hello" },
-    { id: 4, name: "Our man", author: "Hello" },
-    { id: 3, name: "Nexus", author: "Rohit negi" }
-]
-
 app.use(express.json());
 
 app.listen(4000, () => {
     console.log("Sever listen");
 });
 
-// app.get("/book", (req, res) => {
-//     res.send(bookStore);
-// });
+const foodMenu = [
+    { id: 1, food: "Chowmein", category: "veg", price: 500 },
+    { id: 2, food: "Button naan", category: "veg", price: 100 },
+    { id: 3, food: "Chicken", category: "non veg", price: 1500 },
+    { id: 4, food: "Mutton", category: "non veg", price: 700 },
+    { id: 5, food: "Momos", category: "veg", price: 100 },
+    { id: 6, food: "Chai", category: "veg", price: 50 },
+    { id: 7, food: "Roti", category: "veg", price: 150 },
+    { id: 8, food: "kabab", category: "non veg", price: 500 },
+    { id: 9, food: "Lolipop", category: "veg", price: 100 },
+    { id: 10, food: "Curry", category: "veg", price: 400 }
+];
 
-app.get("/book/:id", (req, res) => {
-    console.log(req.params);
-    console.log(typeof req.params);
-    console.log(typeof req.params.id);
-    const id = parseInt(req.params.id);
-    console.log(id);
-    console.log(typeof id);
-    const book = bookStore.find(info => info.id == id);
-    console.log(book)
-    res.send(book);
-});
+const AddToCard = [];
 
-app.post("/book", (req, res) => {
-    console.log(req.body);
-    bookStore.push(req.body);
-    res.send("Data save successfully");
+app.get("/food", (req, res) => {
+    res.status(200).send(foodMenu);
 })
 
-app.patch("/book", (req, res) => {
-    console.log(req.body);
-
-    const id = parseInt(req.body.id);
-    const book = bookStore.find(info => info.id === id);
-
-    if (req.body.author)
-        book.author = req.body.author;
-
-    if (req.body.name)
-        book.name = req.body.name;
-    res.send("patch updated successfully");
-})
-
-app.put("/book", (req, res) => {
-    const id = parseInt(req.body.id);
-    const book = bookStore.find(info => info.id === id);
-    book.author = req.body.author;
-    book.name = req.body.name;
-    res.send("put successfully")
-})
-
-
-app.delete("/book/:id", (req, res) => {
-
-    const id = parseInt(req.params.id);
-    console.log(id);
-
-
-    const index = bookStore.findIndex(info => info.id === id);
-
-    if (index === -1) {
-        return res.status(404).send("Book not found");
+app.post("/admin", (req, res) => {
+    const token = "ABC";
+    const access = token === "ABC" ? 1 : 0;
+    if (access) {
+        foodMenu.push(req.body);
+        res.status(201).send("Item add successuflly");
     }
-    console.log(index)
-    bookStore.splice(index, 1);
-    res.send("Delete successfully")
+    else {
+        res.status(203).send("Not a valid admin");
+    }
 })
 
+app.delete("/admin/:id", (req, res) => {
+    const token = "ABC";
+    const access = token === "ABC" ? 1 : 0;
+    if (access) {
+        const id = parseInt(req.params.id);
+        const index = foodMenu.findIndex(item => item.id === id);
+        if (index === -1) {
 
-app.get("/book", (req, res) => {
-    console.log(req.query);
-    const bookName = bookStore.filter(info => info.author === req.query.author);
-    res.send(bookName);
-});
+            res.status(200).send("not present in the dataset")
+        }
+        else {
+            foodMenu.splice(index, 1);
+            res.status(200).send("successfully deleted")
+        }
+    }
+    else {
+        res.status(403).send("Admin is not verify")
+    }
+})
 
-// app.get("/book", (req, res) => {
-//     console.log(req.query); // e.g. { author: 'Hello' }
-
-//     const bookName = bookStore.find(info => info.author === req.query.author);
-
-//     // if (!bookName) {
-//     //     return res.status(404).send("Book not found");
-//     // }
-
-//     res.send(bookName);
-// });
+app.patch("/admin", (req, res) => {
+    const token = "ABC";
+    const access = token === "ABC" ? 1 : 0;
+    if (access) {
+        const id = parseInt(req.body.id);
+        const foodData = foodMenu.find(item => item.id === id);
+        if (foodData) {
+            if (req.body.food)
+                foodData.food = req.body.food;
+            if (req.body.price)
+                foodData.price = req.body.price;
+            if (req.body.category)
+                foodData.category = req.body.category;
+            res.send("Data Updated SucessFully");
+        }
+        else {
+            res.status().send("not present in the dataset")
+        }
+    }
+    else {
+        res.status(403).send("Admin is not verify")
+    }
+})
---
+++ day09/middleware/auth.js
@@ -0,0 +1,1 @@
+const Auth =<P></P>
